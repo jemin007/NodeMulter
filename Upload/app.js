a050117191c3fd84a8a9cd3ffb99cf767f3952bc
jemin <jeminshrestha9@gmail.com>
2020-04-02 21:24:56 +0545

    single and multiple done

--- Upload/app.js
+++ Upload/app.js
@@ -15,9 +15,9 @@ mongoose.connect(process.env.DATABASE_LOCAL,{
     useCreateIndex: true
 })
 
-let imageSchema = new mongoose.Schema(
-    imageUrl = String
-)
+let imageSchema = new mongoose.Schema({
+    imageUrl : String,
+})
 
 let Picture = mongoose.model('Picture', imageSchema);
 
@@ -33,7 +33,96 @@ app.get('/upload',(req,res)=>{
 })
 
 app.get('/', (req,res)=>{
-    res.render('index')
+    Picture.find({})
+     .then(images=>{
+         res.render('index', {images: images});
+     })
+    
+})
+
+//Single upload
+let storage = multer.diskStorage({
+    destination: './public/uploads/images',
+    filename: (req, file, cb)=>{
+        cb(null,file.originalname);
+    }
+});
+
+let upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb)=>{
+        checkFileType(file,cb);
+    }
+})
+
+
+function checkFileType(file, cb){
+    let fileType = /jpg|jpeg|png|gif/;
+    let extname = fileType.test(path.extname(file.originalname).toLowerCase());
+
+    if(extname){
+        return cb(null,true);
+    }
+    else {
+        return cb('Please select image file only');
+    }
+}
+
+
+app.post('/uploadsingle', upload.single('singleImage'), (req,res,next)=>{
+    const file = req.file;
+
+    if(!file){
+        return console.log("Please select a Image file");
+    }
+
+    let url = file.path.replace('public', '');
+
+    Picture.findOne({ imageUrl: url })
+        .then(img =>{
+            if(img){
+                console.log('Image already exists');
+             return res.redirect('/upload');
+            }
+
+            Picture.create({ imageUrl: url })   
+            .then(img=>{
+                console.log('Image succesfully saved!');
+                res.redirect('/');
+            })
+
+        })
+
+        .catch(err=>{
+            console.log(err);
+        });
+});
+
+//MULTIPLE images
+app.post('/uploadmultiple', upload.array('multipleImages'),(req,res,next)=>{
+    const files = req.files;
+
+    if(!files){
+        return console.log('Please select images')
+    }
+
+    files.forEach(file=>{
+        let url = file.path.replace('public', '');
+        Picture.findOne({ imageUrl : url})
+         .then(async img=>{
+             if(img){
+                 return console.log('Duplicate image');
+                 
+             }
+
+            await Picture.create({ imageUrl : url });
+         })
+         .catch(err =>{
+                return console.log(err);
+         })
+    });
+    res.redirect('/');
+
 })
 
 const port = process.env.PORT;
